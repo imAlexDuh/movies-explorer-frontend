b351cfb882ac3e264d850fcef97258b77d600a25
imAlexDuh <4uva40k99@gmail.com>
2023-12-16 02:48:59 +0300

    Header, Navigation, Footer added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import './Header.css';
+import { Link, useLocation } from 'react-router-dom';
+import Navigation from '../Navigation/Navigation';
+import logo from '../../images/logotype.svg';
+
+export default function Header({ loggedIn, handleCircleClick, isCircleOpened }) {
+
+    const headerEndpoints = ['/movies', '/saved-movies', '/profile', '/'];
+    const location = useLocation();
+
+    if (headerEndpoints.includes(location.pathname))
+        return (
+            <header
+                className={`section header ${location.pathname === '/' && 'header_theme_dark'
+                    }`}>
+                <div className='section__container header__container'>
+                    <Link to='/' className='logo link'>
+                        <img src={logo} alt="лого" />
+                    </Link>
+                    <Navigation
+                        loggedIn={loggedIn}
+                        handleCircleClick={handleCircleClick}
+                        isCircleOpened={isCircleOpened}
+                    />
+                </div>
+            </header>
+        )
+    else
+        return ('');
+}
